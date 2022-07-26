4fa01c7daf9959405b1b66b10eccef5b4f239b7e
portuna85 <portuna85@gmail.com>
2022-07-26 16:44:26 +0900

    AWS Commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -42,5 +42,6 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
 echo "> JAR Name : $JAR_NAME"
 
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties \
+  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
+  -Dspring.profiles.active=real \
   $REPOSITORY/$JAR_NAME 2>&1 &
