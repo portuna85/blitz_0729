3c1624978b37901d524a69a36a2f985a24929666
portuna85 <portuna85@gmail.com>
2022-07-27 21:07:11 +0900

    AWS Commit

--- scripts/start.sh
+++ scripts/start.sh
@@ -21,13 +21,14 @@ echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ubuntu/app/application-oauth.properties,/home/ubuntu/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
