78a8b8fafe51feea9ea35c75b94cdcad8bc59342
portuna85 <portuna85@gmail.com>
2022-07-28 14:30:58 +0900

    application Update

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,23 +7,22 @@ source ${ABSDIR}/profile.sh
 REPOSITORY=/home/ubuntu/app/step3
 PROJECT_NAME=blitz
 
-echo "> Build 파일 복사"
-
+echo "> Build 파일복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 어플리케이션 배포"
+echo "> 새 애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR Name: $JAR_NAME"
+echo "> Jar Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-sudo chmod +x $JAR_NAME
+chmod +x $JAR_NAME
 
-echo "> $JAR_NAME 실행"
+echo "> JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
