3b8133fb7b3e5d3416234ced631a3606394b182a
portuna85 <portuna85@gmail.com>
2022-07-29 09:07:20 +0900

    application Update

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,9 +1,5 @@
 var main = {
-<<<<<<< HEAD
-    init: function () {
-=======
     init : function () {
->>>>>>> 89c0ce7 (Initial commit)
         var _this = this;
         $('#btn-save').on('click', function () {
             _this.save();
@@ -17,11 +13,7 @@ var main = {
             _this.delete();
         });
     },
-<<<<<<< HEAD
-    save: function () {
-=======
     save : function () {
->>>>>>> 89c0ce7 (Initial commit)
         var data = {
             title: $('#title').val(),
             author: $('#author').val(),
@@ -32,26 +24,16 @@ var main = {
             type: 'POST',
             url: '/api/v1/posts',
             dataType: 'json',
-<<<<<<< HEAD
-            contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function () {
-=======
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
->>>>>>> 89c0ce7 (Initial commit)
             alert('글이 등록되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     },
-<<<<<<< HEAD
-    update: function () {
-=======
     update : function () {
->>>>>>> 89c0ce7 (Initial commit)
         var data = {
             title: $('#title').val(),
             content: $('#content').val()
@@ -61,55 +43,33 @@ var main = {
 
         $.ajax({
             type: 'PUT',
-<<<<<<< HEAD
-            url: '/api/v1/posts/' + id,
-            dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function () {
-=======
             url: '/api/v1/posts/'+id,
             dataType: 'json',
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
->>>>>>> 89c0ce7 (Initial commit)
             alert('글이 수정되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     },
-<<<<<<< HEAD
-    delete: function () {
-=======
     delete : function () {
->>>>>>> 89c0ce7 (Initial commit)
         var id = $('#id').val();
 
         $.ajax({
             type: 'DELETE',
-<<<<<<< HEAD
-            url: '/api/v1/posts/' + id,
-            dataType: 'json',
-            contentType: 'application/json; charset=utf-8'
-        }).done(function () {
-=======
             url: '/api/v1/posts/'+id,
             dataType: 'json',
             contentType:'application/json; charset=utf-8'
         }).done(function() {
->>>>>>> 89c0ce7 (Initial commit)
             alert('글이 삭제되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> 89c0ce7 (Initial commit)
 };
 
 main.init();
