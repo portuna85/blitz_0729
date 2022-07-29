3deb96c609d45fd658b81c9d24aaab073098a6d4
portuna85 <portuna85@gmail.com>
2022-07-29 19:45:51 +0900

    application Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,13 +25,13 @@ echo "> 새 애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR Name : $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-sudo chmod +x $JAR_NAME
+chmod +x $JAR_NAME
 
-echo "> $JAR_NAME"
+echo "> $JAR_NAME 실행"
 
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
