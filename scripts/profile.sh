e6d20db582d2bea02666a8c8d82f43158968a5f3
portuna85 <portuna85@gmail.com>
2022-07-30 16:27:31 +0900

    application Update

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -12,7 +12,8 @@ function find_idle_profile() {
     CURRENT_PROFILE=$(curl -s http://localhost/profile)
   fi
 
-  if [ ${CURRENT_PROFILE} == real1 ]; then
+  if [ ${CURRENT_PROFILE} == real1 ]
+  then
     IDLE_PROFILE=real2
   else
     IDLE_PROFILE=real1
@@ -25,7 +26,8 @@ function find_idle_profile() {
 function find_idle_port() {
   IDLE_PROFILE=$(find_idle_profile)
 
-  if [ ${IDLE_PROFILE} == real1 ]; then
+  if [ ${IDLE_PROFILE} == real1 ]
+  then
     echo "8081"
   else
     echo "8082"
