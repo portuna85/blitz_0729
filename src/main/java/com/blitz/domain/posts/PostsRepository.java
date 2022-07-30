fb6ddb3eb4db45535e6c2521851920a5fc5344b7
portuna85 <portuna85@gmail.com>
2022-07-30 19:39:33 +0900

    application Update

--- src/main/java/com/blitz/domain/posts/PostsRepository.java
+++ src/main/java/com/blitz/domain/posts/PostsRepository.java
@@ -1,6 +1,7 @@
 package com.blitz.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -9,4 +10,8 @@ public interface PostsRepository extends JpaRepository<Posts, Long> {
 
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
+
+    @Modifying
+    @Query("update Posts p set p.view = p.view + 1 where p.id = :id")
+    int updateView(Long id);
 }
