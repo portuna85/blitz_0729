8bfc024cd0fa92898fd369189ba764ccb951f9f5
portuna85 <portuna85@gmail.com>
2022-07-31 10:31:26 +0900

    application Update

--- src/main/java/com/blitz/domain/user/Role.java
+++ src/main/java/com/blitz/domain/user/Role.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    USER("ROLE_USER"),
-    ADMIN("ROLE_ADMIN"),
-    SOCIAL("ROLE_SOCIAL");
 
-    private final String value;
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+
 }
